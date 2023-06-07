ce8fc2d78ef09bf4df05d3c3f2eca6947a47afc2
Ade Ritchards <aderitchards@gmail.com>
2023-06-07 12:36:08 -0700

    first commit

---
+++ controllers/controllerTemplate.js
@@ -0,0 +1,5 @@
+import { _schemaName_ } from '../models/modelTemplate'
+import mongoose from 'mongoose'
+
+const MODEL_NAME = mongoose.model("_name for db_", _schemaName_);
+
---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyparser from "body-parser";
+import cors from "cors";
+
+const app = new express();
+const PORT = 4015;
+const ATLAL_URL = `mongodb+srv://Ade-user-2:admin123@cluster0.oe5ybr1.mongodb.net/test`;
+
+const COMPAS_URL = `mongodb://127.0.0.1/${ROOT_ROUTE}`;
+
+//mongo connection
+mongoose.Promise = global.Promise
+mongoose.connect(COMPAS_URL,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+//bodyparser setup
+app.use(bodyparser.urlencoded({extended: true}))
+app.use(bodyparser.json())
+//set up cookies parser
+app.use(cookieParser())
+//CORS setup
+app.use(cors())
+
+
+
+app.get('/', (req, res) =>{
+    res.send(
+    `<div style='width: 100vw; height:100vh; display: flex; justify-content: center; align-items: center;'>\
+    <p style= 'font-size: 50px;' >You look lost kid. Go to front end<p/>\
+    </div>`
+    )
+});
+
+app.listen(PORT, ()=>{
+        console.log(`The ${APP_NAME} server is running on port: ${PORT}`)
+        console.log("SERVER IS RUNNING - SUCCESS")
+    }
+);
+
